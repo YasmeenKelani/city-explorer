483a4a54784db48cb03ee095edd8b666b041b7e4
YasmeenKelani <yasminekailani98@gmail.com>
2021-09-23 17:44:43 +0300

    lab07

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import "./App.css";
+
 
 class App extends React.Component {
   constructor(props) {
@@ -8,6 +15,8 @@ class App extends React.Component {
       locationResult: {},
       searchQuery: "",
       showLocInfo: false,
+      showError: false,
+     
     };
   }
 
@@ -17,48 +26,119 @@ class App extends React.Component {
     await this.setState({
       searchQuery: event.target.city.value,
     });
+    console.log(this.state.searchQuery);
+  
 
-    console.log("key", process.env.REACT_APP_LOCATIONIQ_KEY);
+    try {
+      let reqUrl = `${process.env.REACT_APP_SERVER_LINK}/weather?searchQuery=${this.state.searchQuery}`;
+      console.log(reqUrl);
+      let locResult = await axios.get(reqUrl);
+      console.log("locResult", locResult);
+      console.log("seclocResult", locResult.data);
+     
 
-    let reqUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
-
-    let locResult = await axios.get(reqUrl);
-    console.log("locResult", locResult);
-    console.log("seclocResult", locResult.data);
-    console.log("seclocResult", locResult.data[0]);
-
-    this.setState({
-      locationResult: locResult.data[0],
-      showLocInfo: true,
-    });
+      this.setState({
+        locationResult: locResult.data,
+        showLocInfo: true,
+        showError: false,
+      });
+      console.log(this.state.locationResult);
+    } catch {
+      console.log("something went wrong");
+      this.setState({
+        showError: true,
+        showLocInfo: false,
+      });
+    }
   };
 
   render() {
     return (
       <div>
-        <h2 style={{ textAlign:'center', padding:'80px',position:'relative',bottom:'20px', backgroundColor:'aqua'}}>City Explorer app</h2>
-        <form style ={{textAlign:'center'}} onSubmit={this.getLocFun}>
-          <input type="text" name="city" />
-          <input type="submit" value="get city info" />
-        </form>
+        <h2
+          style={{
+            fontWeight: "bold",
+            textAlign: "center",
+            padding: "80px",
+            position: "relative",
+            bottom: "20px",
+            backgroundColor: "blue",
+            color: "white"
+          }}
+        >
+          City Explorer App
+        </h2>
+        <Form
+          style={{
+            textAlign: "center",
+            padding: "100px",
+            width: "100%",
+            backgroundColor: "blue",
+            borderStyle: "outset",
+            borderColor: "black",
+          }}
+          onSubmit={this.getLocFun}
+        >
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label style={{ fontWeight: "bold", color: "white" }}>
+              City Explorer
+            </Form.Label>
+            <Form.Control type="text" name="city" placeholder="Enter city name" />
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
+          <Button 
+          style = {{opacity:"1"}}
+          
+            type="submit"
+          >
+            Explore!
+          </Button>
+          
+        </Form>
 
         {this.state.showLocInfo && (
           <>
-            <p style={{fontWeight:"bold"}}>City name: {this.state.searchQuery}</p>
-            <p style={{fontWeight:"bold"}}>latitude: {this.state.locationResult.lat}</p>
-            <p style={{fontWeight:"bold"}}>longitude: {this.state.locationResult.lon} </p>
-
-            <img
-              src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=10`}
-              alt="city"
-            />
-          </>
+            <h3
+              style={{
+                textAlign: "center",
+                marginTop:"3px",
+              }}
+            >
+              City name: {this.state.searchQuery}
+            </h3>
+            <Row
+              xs={1}
+              md={3}
+              className="g-4"
+              style={{
+                position: "relative",
+                margin: "90px",
+                marginTop: "10px",
+              }}
+            > 
+              <Card border="primary" style={{ width: "25rem", position:"relative", left: "450px" }}>
+                <Card.Header style={{ fontWeight: "bold", textAlign:"center" }}>City</Card.Header>
+                <Card.Body>
+                  <Card.Text>
+                   <p>  Description : {this.state.locationResult[0].description}
+                   <br></br> Date : {this.state.locationResult[0].date} </p>
+                    <p> Description : {this.state.locationResult[1].description}
+                    <br></br> Date : {this.state.locationResult[1].date} </p> 
+                    <p> Description : {this.state.locationResult[2].description}
+                    <br></br> Date : {this.state.locationResult[2].date} </p>
+                   
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              </Row>
+            </>
         )}
-        
+        {this.state.showError && (
+          <p> something wrong in getting location data</p>
+        )}
       </div>
     );
   }
-  
 }
 
 export default App;
